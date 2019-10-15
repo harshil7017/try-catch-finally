c529d75f5ba19cb534c3ca51edf1c62af32b0172
user <harshilsheth@outlook.com>
2019-10-15 18:59:46 +0530

    first change

---
+++ try-catch-finally/src/trycatch/switchst.java
@@ -0,0 +1,27 @@
+package trycatch;
+
+public class switchst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int day=4;
+		switch(day)
+		{
+		case 1:	
+			 System.out.println("MONDAY");
+			break;
+			case 2:
+				 System.out.println("TUE");
+			break;
+			case 3:
+				 System.out.println("WED");
+					break;
+			case 4:
+				 System.out.println("thru");
+					break;
+				
+		}
+	}
+
+}
---
+++ try-catch-finally/src/trycatch/trycatch.java
@@ -0,0 +1,27 @@
+package trycatch;
+
+final class trycatch {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		trycatch tc=new trycatch();
+		tc.vechicle();
+		try
+		{
+			
+			System.out.println("hello");
+		}
+		catch(Exception e)
+		{
+			System.out.println("i m");
+		}
+	}
+	void vechicle()
+	{
+		System.out.println("vechicle is");
+	}
+	
+
+}
